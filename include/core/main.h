9088ec4192296824b0d65d0652ba6d284c2defc1
estebanmatias92 <estebanmatias92@gmail.com>
2022-06-03 14:23:19 -0300

    Set the indentation to four spaces and organize the folders in modules

---
+++ include/core/main.h
@@ -0,0 +1,6 @@
+#ifndef __MAIN__
+#define __MAIN__
+
+    /* Functions declarations here */
+
+#endif
---
+++ src/core/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <main.h>
+
+int main() {
+
+    printf("Hello, World!");
+
+    return 0;
+
+}
